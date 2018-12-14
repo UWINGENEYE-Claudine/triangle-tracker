51b8b5ed3cc8e0178b8fc929dba7eb09c6313447
UWINGENEYE-Claudine <cuwi2020@gmail.com>
2018-12-14 12:00:30 +0200

    add js script

---
+++ script.js
@@ -0,0 +1,12 @@
+function myFunction(){
+    var firstString = document.getElementById("firstSides").value;
+    alert(firstString);
+}
+function myFunction(){
+    var firstString = document.getElementById("secondSides").value;
+    alert(firstString);
+}
+function myFunction(){
+    var firstString = document.getElementById("thirdSides").value;
+    alert(firstString);
+}
